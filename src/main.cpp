2b7580c777fc90140292318bf0122ee31e634f26
o-Haxolotl-o <noahgorme@gmail.com>
2025-03-23 15:35:45 -0400

    remove unnecessary testing physics code

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,13 @@ int main() {
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Fish");
     window.setFramerateLimit(300);
     //Init__________________________________________________________________________________________________________________________________________________________________________
-    const int vertNum = 10; //how many points
-    const int vertSpace = 16;
-    const int baseSize = 6;
+    const int vertNum = 16; //how many points
+    const int vertSpace = 18; //distance between points
+    const int baseSize = 6; //head size
+    const float decreaseSize = .2; //how fast to decrease vert size
 
-    const int eyeSpace = 90;
-    const int eyeRadius = 20;
+    const int eyeSpace = 90; //how far apart
+    const int eyeRadius = 20; //eye size
 
     sf::Texture gradientTexture("../../src/Images/Gradient.png"); //gradient to be used in the metaball
     std::vector<sf::Sprite> c(vertNum, sf::Sprite(gradientTexture)); //list of vectors for fish part sprite
@@ -23,7 +24,7 @@ int main() {
         c[i].setOrigin( sf::Vector2f(1024 * c[i].getScale().x / 2,1024 * c[i].getScale().y / 2) );
         c[i].setPosition(sf::Vector2f(window.getSize().x/2, window.getSize().y/2));
 
-        c[i].setScale(sf::Vector2f(baseSize * .1 / ((i*0.5)+1.5), baseSize * .1 / ((i*0.5)+1.5)));
+        c[i].setScale(sf::Vector2f(baseSize * .1 / ((i*decreaseSize)+1.5), baseSize * .1 / ((i*decreaseSize)+1.5)));
         c[i].setColor(sf::Color::White);
     }
 
@@ -31,12 +32,6 @@ int main() {
     sf::CircleShape eyes{eyeRadius};
     eyes.setOrigin(sf::Vector2f(eyeRadius, eyeRadius));
     eyes.setFillColor(sf::Color(0, 92, 184,255));
-
-    sf::Vector2f cd[vertNum];
-    for (int i = 0; i < vertNum; i++) {
-        //cd[i].x = i*5;
-        //cd[i].y = i;
-    }
 
 
     sf::Shader metaballShader; //threshold shader to make metaballs
@@ -57,29 +52,6 @@ int main() {
             }
         }
 
-        //forces
-        for (int i = 0; i < vertNum; i++) {
-            //cd[i].y+=0.2; //gravity
-            cd[i].x*=0.95;//friction--
-            cd[i].y*=0.95;
-        }
-
-
-        //bounce
-        for (int i = 0; i < vertNum; i++) {
-            if (c[i].getPosition().y > window.getSize().y) {
-                cd[i].y *=-0.9;
-                c[i].setPosition(sf::Vector2f(c[i].getPosition().x, window.getSize().y+1));
-            }
-            if (c[i].getPosition().x < 0) {
-                cd[i].x *=-0.9;
-                c[i].setPosition(sf::Vector2f(0, c[i].getPosition().y));
-            }
-            if (c[i].getPosition().x > window.getSize().x) {
-                cd[i].x *=-0.9;
-                c[i].setPosition(sf::Vector2f(window.getSize().x, c[i].getPosition().y));
-            }
-        }
         //distance constraint
         for (int i = 0; i < vertNum; i++) {
             if (i < vertNum - 1) {
@@ -102,23 +74,20 @@ int main() {
                     eyesPosition = sf::Vector2f(normVector.x,normVector.y);
             }
         }
+        
 
-        //apply force
-        for (int i = 0; i < vertNum; i++) {
-            c[i].setPosition(c[i].getPosition()+cd[i]);
-        }
         c[0].setPosition((sf::Vector2f)sf::priv::InputImpl::getMousePosition()); //move head to mouse position
 
         //Draw__________________________________________________________________________________________________________________________________________________________________________
         window.clear(sf::Color(24, 38, 66,255)); //clear screen and metaball render texture
-        metaballTexture.clear(sf::Color::Transparent);
+        metaballTexture.clear(sf::Color::Transparent); //reset metaballs
 
-        for (int i = 0; i < vertNum; i++) { //render fish to render texture
+        for (int i = 0; i < vertNum; i++) { //draw fish to render texture
                 metaballTexture.draw(c[i]);
         }
-        metaballTexture.display();
+        metaballTexture.display(); //finish metaball render
 
-        window.draw(metaballSprite, &metaballShader);
+        window.draw(metaballSprite, &metaballShader); //draw metaballs to screen
 
         //set eye to position for both sides--
         eyes.setPosition(sf::Vector2f(c[0].getPosition().x + eyesPosition.y * eyeSpace,c[0].getPosition().y + eyesPosition.x * -eyeSpace));
